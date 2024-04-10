f7486cf806d30239b592c93c63e350307d66d0c0
vitorkuhn <vitorak2000@gmail.com>
2024-04-09 21:53:20 -0300

    criação do botão

--- WinFormsApp/Form1.cs
+++ WinFormsApp/Form1.cs
@@ -8,14 +8,14 @@ namespace WinFormsApp
         private const int MOUSEEVENT_LEFTDOWN = 0x0002;
         private const int MOUSEEVENT_LEFTUP = 0x0004;
         // metodo que execeuta o evento de click
-        [DllImport("user32.dll", CharSet = CharSet.Auto,CallingConvention = CallingConvention.StdCall)]
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
         public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
 
         //gerar as posicoes aleatorias
         private Random random = new Random();
 
         //movimento aleatorio do mouse
-        
+
         private void MoverCursor()
         {
             //largura da tela
@@ -33,6 +33,37 @@ namespace WinFormsApp
         public Form1()
         {
             InitializeComponent();
+        }
+        // no evento de clique
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //para i igual a 0 até i menor que 10 repita
+            for (int i = 0; i < 100; i++)
+            {
+                //chamando evento que move o cursor aleatorio
+                MoverCursor();
+
+                for(int clique = 1; clique <= 2; clique++)
+                {
+                    MouseClicar();
+                }
+
+                //cerebro.dormir 1 segundo
+                Thread.Sleep(100);
+
+            }
+        }
+        private void MouseClicar()
+        {
+            mouse_event(
+                MOUSEEVENT_LEFTDOWN, //codigo do evento pressionar
+                Cursor.Position.X,  //posicao x do cursor
+                Cursor.Position.Y, 0, 0); //posicao y do cursor
+
+            mouse_event(
+                MOUSEEVENT_LEFTUP, //codigo do evento de soltar o botão do mouse
+                Cursor.Position.X, //posicao x do cursor
+                Cursor.Position.Y, 0, 0); //posicao y do cursor
         }
     }
 }
